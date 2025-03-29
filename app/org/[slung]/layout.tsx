4dd37c2d2d4dbfb6bd9b7bc1c7affa15ad28dca4
adelsultann <maax.1992@gmail.com>
2025-03-29 19:31:13 +0300

    reay without auth and org logo

--- app/org/[slung]/layout.tsx
+++
@@ -1,5 +0,0 @@
-// import { OrgProvider } from "../../context/OrgContext"; // from layout.tsx
-
-// export default function OrgLayout({ children }: { children: React.ReactNode }) {
-//   return <OrgProvider>{children}</OrgProvider>;
-// }
