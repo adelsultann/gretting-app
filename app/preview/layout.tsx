791a61b4ac7c595bf29a56d66c11e15e9d39f045
adelsultann <maax.1992@gmail.com>
2025-03-29 20:33:57 +0300

    rady without auth and org log in

--- app/preview/layout.tsx
+++ app/preview/layout.tsx
@@ -1,6 +1,7 @@
 // app/preview/layout.tsx
 
 // this page only for vercel deploy 
+//Prevents runtime crash on Vercel due to useSearchParams() or usePathname()
 import { ReactNode, Suspense } from "react";
 
 export default function PreviewLayout({ children }: { children: ReactNode }) {
