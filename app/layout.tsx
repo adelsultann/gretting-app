eaf29da91f7e41f0d14ed62a09053abe82d04784
adelsultann <adelalsultan647@gmail.com>
2025-05-18 18:52:38 +0300

    done with the admin dashboard

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,20 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import Script from "next/script";
 import "./globals.css";
+import { GoogleTagManager } from '@next/third-parties/google'
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
 
-const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
-const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "تهنئتي",
-  description: "أنشئ بطاقات تهنئة جميلة ومخصصة للمناسبات الخاصة مثل عيد الفطر، عيد الأضحى وغيرها",
+  description: " أنشىءبطاقات تهنئة جميلة ومخصصة للمناسبات الخاصة مثل عيد الفطر، عيد الأضحى وغيرها",
 };
 
 export default function RootLayout({
@@ -17,24 +23,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ar">
-      <head>
-        {/* Load GA script */}
-        <Script
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
-          strategy="afterInteractive"
-        />
-        {/* Initialize GA */}
-        <Script id="ga-init" strategy="afterInteractive">
-          {`window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);} 
-gtag('js', new Date());
-gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', { page_path: window.location.pathname });`}
-        </Script>
-      </head>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-
-        {children}
+    <html lang="en">
+       <GoogleTagManager gtmId='GTM-NH3MH55B'
+       />
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+       
+          {children}
+       
       </body>
     </html>
   );
